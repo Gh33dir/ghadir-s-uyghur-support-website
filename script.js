51c71770af1b36473540f5e1f4797e1393e71b06
Ghadir <>
2022-08-08 17:42:35 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+
+var displayName = document.getElementById("secret");
+
+var other = document.getElementById("myText");
+
+var button = document.getElementById("myButton");
+
+button.addEventListener("click", dis);
+
+
+function dis(){
+  
+  displayName.innerHTML= "Hello my name is "+ other.value + ", I'd like to adress you on an Issue today, and ask for your help. Please support the Uyghurs and their human rights. Without your support, Uyghur refugees will be sent back to concentration camps in China. I stronly hope for your support and will urge the reconciliation conference comittee to keep the Uyghur Human rights Protection Act in the final version of the America COMPETES act! It is Section 30306 in the House version. Uyghurs are suffering genocide for the fifth year now, While the US took great leadership on sanctions on china, it is still unable to process refugee applications from the Uyghurs who have ran away from the concentration camps to escape from genoicde. This is due to the UNHCR processing being ineffective or absent in some countries where they live in now. The Uyghur Human Rights Protection Act (H.R.1630/S.1080) enables the U.S. to rescue refugees *before* they are deported to China"
+    }
+//other button
+
+
+
+
+
---
+++ script2.js
@@ -0,0 +1,7 @@
+var redirect = document.getElementById("redirect-button");
+
+redirect.addEventListener("click", changeScreen);
+
+function changeScreen() {
+  location.href = "https://ghadirs-uyghur-support-website.xxadirxx.repl.co/support.html";
+}
